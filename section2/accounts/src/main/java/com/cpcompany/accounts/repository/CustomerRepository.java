3b1cb278008acf45317aeab9ef3d96edc19c7666
chandanp11 <39085343+chandanp11@users.noreply.github.com>
2024-10-20 00:56:21 +0530

    created accounts service and implemented JPA

---
+++ section2/accounts/src/main/java/com/cpcompany/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.cpcompany.accounts.repository;
+
+import com.cpcompany.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
